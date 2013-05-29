9ecf787162ec486c6d37a07bb2a0fa0acd314470
aldorp <aldorp@aldorp-Mia.(none)>
2013-05-29 03:45:48 -0600

    creando aplicasion de status en rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Probando::Application.config.secret_token = 'c6c9d73ba4b72df82d7872fdd1d8156f456a00b60670ec9d241abfa47f322427d512b24eb97561923b3eb3f6820f5b9de2fe9a92374352918291a9bb3cd74629'
